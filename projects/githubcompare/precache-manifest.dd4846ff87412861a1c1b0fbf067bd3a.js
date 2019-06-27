4fba1c22d9ebb5c2580e7de5fc50e3ee3574a255
Rodrigo Dias <diasbass@hotmail.com>
2019-06-27 14:52:28 -0300

    Add project - GitCompare

---
+++ projects/githubcompare/precache-manifest.dd4846ff87412861a1c1b0fbf067bd3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be07167dfff41a973e5d0282bfd69566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b2a2b8add28fd7bad13",
+    "url": "./static/css/2.b1e91090.chunk.css"
+  },
+  {
+    "revision": "3b2a2b8add28fd7bad13",
+    "url": "./static/js/2.4306a1cc.chunk.js"
+  },
+  {
+    "revision": "89c84fbea2ed8037ed22",
+    "url": "./static/js/main.3c3882a8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
