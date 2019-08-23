e6a322c60b8056274bed2dc068a81ef80b741724
Rodrigo Dias <diasbass@hotmail.com>
2019-08-23 17:39:31 -0300

    Atualizando projeto de cadastro

---
+++ projects/reactcadastro/precache-manifest.dca4c893d0b802a38b107031818101d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5207eddd5a097297ac135528ad2a44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d49afa97b24320d02561",
+    "url": "./static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "d49afa97b24320d02561",
+    "url": "./static/js/2.c079e5ed.chunk.js"
+  },
+  {
+    "revision": "49551c9052f3be41bdd0",
+    "url": "./static/js/main.1ba37083.chunk.js"
+  },
+  {
+    "revision": "5cb43c3ee96e44ba7af6",
+    "url": "./static/js/runtime~main.501d145d.js"
+  }
+]);
--- projects/reactcadastro/service-worker.js
+++ projects/reactcadastro/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a330d27f3d05bd1f9b5400051b8fcd68.js"
+  "./precache-manifest.dca4c893d0b802a38b107031818101d5.js"
 );
 
 self.addEventListener('message', (event) => {
