c8931d82de018ecd12a85123459d28b065633bcb
Rodrigo Dias <diasbass@hotmail.com>
2019-08-23 14:54:08 -0300

    Adicionando projeto - React Cadastro

---
+++ projects/reactcadastro/precache-manifest.a330d27f3d05bd1f9b5400051b8fcd68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e2e4094b4332f95baef1722249c431",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1151d26f54346e6e663b",
+    "url": "./static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "1151d26f54346e6e663b",
+    "url": "./static/js/2.853458a3.chunk.js"
+  },
+  {
+    "revision": "c33151c9b7847da833f4",
+    "url": "./static/js/main.54186ad8.chunk.js"
+  },
+  {
+    "revision": "5cb43c3ee96e44ba7af6",
+    "url": "./static/js/runtime~main.501d145d.js"
+  }
+]);
