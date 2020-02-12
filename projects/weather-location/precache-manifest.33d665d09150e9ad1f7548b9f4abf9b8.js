842e599d362f5991ceaaad69780c2f47fc0724ea
Rodrigo Dias <diasbass@hotmail.com>
2020-02-12 13:20:08 -0300

    correcao projeto weather

---
+++ projects/weather-location/precache-manifest.33d665d09150e9ad1f7548b9f4abf9b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f949e8d8ba8bfbd509de77ebf3a7e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a7148b73983032aeb6c",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b62c7c295421a21c7cad",
+    "url": "./static/js/2.6e7b1102.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.6e7b1102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a7148b73983032aeb6c",
+    "url": "./static/js/main.9d81f3cd.chunk.js"
+  },
+  {
+    "revision": "f1d47b68f0b681fc01cf",
+    "url": "./static/js/runtime-main.ca8d04de.js"
+  }
+]);
--- projects/weather-location/service-worker.js
+++ projects/weather-location/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.858d585500a7d2b796a2fccd6335eedb.js"
+  "./precache-manifest.33d665d09150e9ad1f7548b9f4abf9b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
