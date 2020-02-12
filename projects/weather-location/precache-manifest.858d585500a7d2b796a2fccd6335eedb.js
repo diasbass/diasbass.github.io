2ffec7ebc7a3ca8884eac82ce98b8580e159ad80
Rodrigo Dias <diasbass@hotmail.com>
2020-02-12 11:19:15 -0300

    Add project Wheater Location

---
+++ projects/weather-location/precache-manifest.858d585500a7d2b796a2fccd6335eedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9eb40515d3ade7e1ecf8368bbb5818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01050c650b0a1f99dadd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b62c7c295421a21c7cad",
+    "url": "/static/js/2.6e7b1102.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6e7b1102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01050c650b0a1f99dadd",
+    "url": "/static/js/main.d0b5b098.chunk.js"
+  },
+  {
+    "revision": "67f425e8a08153b3aa92",
+    "url": "/static/js/runtime-main.526d377b.js"
+  }
+]);
